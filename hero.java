fdbbf5fc99504bb403f7100212a118529689e2b6
tk-955990 <105995713+tk-955990@users.noreply.github.com>
2022-05-29 07:10:04 +0900

    kadai7
    2022/5/29/am7

--- hero.java
+++ hero.java
@@ -1,7 +1,27 @@
 package kadai;
 
-public class hero {
+public class Hero {
 	
 	String name ;
-
+    int hp ;
+    int at ;
+	int df ;
+	
+	
+	void attack() {
+		System.out.println(this.name+"の攻撃！！");
+		System.out.println("怪物に"+this.at+"ポイントのダメージを与えた！！");
+	}
+	
+	void defense() {
+		System.out.println(this.name+"は身を守った");
+		this.df=this.df+10;
+		System.out.println("防御力が１０上がり、防御力が"+this.df+"ポイントになった");
+	}	
+	
+	void heal() {
+		System.out.println(this.name+"は休憩した");
+		this.hp=this.hp+10;
+		System.out.println(this.name+"の体力が１０回復し、"+this.hp+"ポイントになった");
+	}
 }
